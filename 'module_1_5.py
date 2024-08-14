9dc2967f6815ce7a54f92ea5bb07c3f724b31cf1
Balhash-old <tel-alex@mail.ru>
2024-08-14 15:47:52 +0300

    Add files via upload

---
+++ 'module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var=1, 2, 3 , 'lion','exe'
+print(immutable_var)
+#immutable_var[1]=45                        #кортеж хранит ссылку на список, а не сам список
+#print(immutable_var)
+mutable_list=['doctor',' soilder', 'teacher','driver']
+print(mutable_list)
+mutable_list[2]='engineer'
+print(mutable_list)
